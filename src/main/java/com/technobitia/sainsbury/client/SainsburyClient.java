eb74b46f0f2223837a1bca54906af2a00665d1be
Jose David Baena <josedab@gmail.com>
2015-02-15 23:30:08 +0000

    NT Recipe model and Sainsbury client adapted to the model - Sainsbury client will return a recipe object

--- src/main/java/com/technobitia/sainsbury/client/SainsburyClient.java
+++ src/main/java/com/technobitia/sainsbury/client/SainsburyClient.java
@@ -10,26 +10,52 @@ import org.jsoup.nodes.Document;
 import com.technobitia.sainsbury.exceptions.SainsburyException;
 import com.technobitia.sainsbury.extractors.Extractor;
 import com.technobitia.sainsbury.extractors.impl.IngredientsExtractor;
+import com.technobitia.sainsbury.extractors.impl.IngredientsListExtractor;
+import com.technobitia.sainsbury.extractors.impl.NutritionFactsExtractor;
+import com.technobitia.sainsbury.extractors.impl.RecipeMethodExtractor;
+import com.technobitia.sainsbury.extractors.impl.TitleExtractor;
+import com.technobitia.sainsbury.model.Recipe;
 import com.technobitia.sainsbury.request.SainsburyRequest;
 
 public class SainsburyClient {
-    Extractor ingredientsExtractor = new IngredientsExtractor();
+    private Extractor titleExtractor;
+    private Extractor ingredientsExtractor;
+    private Extractor ingredientListExtractor;
+    private Extractor nutritionFactsExtractor;
+    private Extractor stepsRecipeExtractor;
+    
     public SainsburyClient() {
-        
+        titleExtractor = new TitleExtractor();
+        ingredientsExtractor = new IngredientsExtractor();
+        ingredientListExtractor = new IngredientsListExtractor();
+        nutritionFactsExtractor = new NutritionFactsExtractor();
+        stepsRecipeExtractor = new RecipeMethodExtractor();
     }
     
-    public String extractInformation(SainsburyRequest request) throws SainsburyException {
+    public Recipe extractInformation(SainsburyRequest request) throws SainsburyException {
         checkNotNull(request);
-        String result = null;
+        Recipe recipe = null;
 
         String url = request.getUrl();
         try {
             Document doc = Jsoup.connect(url).get();
-            result = ingredientsExtractor.extract(request, doc);
+            String ingredientsToBuy = ingredientsExtractor.extract(request, doc);
+            String title = titleExtractor.extract(request, doc);
+            String ingredientsList = ingredientListExtractor.extract(request, doc);
+            String nutritionFacts = nutritionFactsExtractor.extract(request, doc);
+            String stepsToCook = stepsRecipeExtractor.extract(request, doc);
+            
+            recipe = new Recipe.Builder(url)
+                               .withTitle(title)
+                               .withIngredientsList(ingredientsList)
+                               .withIngredientsToBuy(ingredientsToBuy)
+                               .withNutritionFacts(nutritionFacts)
+                               .withStepsToCook(stepsToCook)
+                               .build();
         } catch (IOException e) {
             throw new SainsburyException("I/O exception when requesting information to url:" + url);
         }
         
-        return result;
+        return recipe;
     }
 }
---
+++ src/main/java/com/technobitia/sainsbury/model/Recipe.java
@@ -0,0 +1,82 @@
+package com.technobitia.sainsbury.model;
+
+
+public class Recipe {
+    private String url;
+    private String title;
+    private String ingredientsToBuy;
+    private String ingredientsList;
+    private String nutritionFacts;
+    private String stepsToCook;
+    
+    private Recipe(Builder builder) {
+        url = builder.url;
+        title = builder.title;
+        ingredientsToBuy = builder.ingredientsToBuy;
+        ingredientsList = builder.ingredientsList;
+        nutritionFacts = builder.nutritionFacts;
+        stepsToCook = builder.stepsToCook;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getIngredientsToBuy() {
+        return ingredientsToBuy;
+    }
+    public String getIngredientsList() {
+        return ingredientsList;
+    }
+    public String getNutritionFacts() {
+        return nutritionFacts;
+    }
+    public String getStepsToCook() {
+        return stepsToCook;
+    }
+    
+    public static class Builder {
+        private final String url;
+        private String title;
+        private String ingredientsToBuy;
+        private String ingredientsList;
+        private String nutritionFacts;
+        private String stepsToCook;
+
+        public Builder(String url) {
+            this.url = url;
+        }
+        
+        public Builder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+        
+        public Builder withIngredientsToBuy(String ingredientsToBuy) {
+            this.ingredientsToBuy = ingredientsToBuy;
+            return this;
+        }
+        
+        public Builder withIngredientsList(String ingredientsList) {
+            this.ingredientsList = ingredientsList;
+            return this;
+        }
+        
+        public Builder withNutritionFacts(String nutritionFacts) {
+            this.nutritionFacts = nutritionFacts;
+            return this;
+        }
+        
+        public Builder withStepsToCook(String stepsToCook) {
+            this.stepsToCook = stepsToCook;
+            return this;
+        }
+
+        public Recipe build() {
+            return new Recipe(this);
+        }
+    }
+    
+}
